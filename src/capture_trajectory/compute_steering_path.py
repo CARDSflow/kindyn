75028c6377838d3608bfab3a1e64066304f09987
Carl Martin Krokeide <carlmk@stud.ntnu.no>
2019-01-11 17:21:39 +0100

    Initial steps on steering controller

---
+++ src/capture_trajectory/compute_steering_path.py
@@ -0,0 +1,3 @@
+# Program tham computes [x, y, z] coordinates of left and right endeffector for steering movements
+
+# Alternatively take steering angle as input, produce [x, y, z] coordinates for endeffectors as output
