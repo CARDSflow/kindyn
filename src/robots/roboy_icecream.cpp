e46fd2bd2fa49132ef919f0f6ce5f07e92e3ec9d
Arne Sachtler <arne.sachtler@gmail.com>
2019-08-31 23:19:54 +0200

    Call init after reading of the pose

--- src/robots/roboy_icecream.cpp
+++ src/robots/roboy_icecream.cpp
@@ -63,9 +63,9 @@ public:
         }
         vector<string> joint_names;
         nh->getParam("joint_names", joint_names);
-        init(urdf,cardsflow_xml,joint_names);
         nh->getParam("external_robot_state", external_robot_state);
         ROS_INFO_STREAM("External robot state: " << external_robot_state);
+        init(urdf,cardsflow_xml,joint_names);
         update();
         motor_command = nh->advertise<roboy_middleware_msgs::MotorCommand>("/roboy/middleware/MotorCommand",1);
         motor_status_sub = nh->subscribe("roboy/middleware/MotorStatus",1,&RoboyIcecream::MotorStatus, this);
