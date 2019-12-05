cbfa941e58ec59f8c33347aa68f0000f0dedd004
letrend <st@gi.ai>
2019-12-05 19:20:55 +0100

    with trajectory checkpoints

--- scripts/trajectoryControl.py
+++ scripts/trajectoryControl.py
@@ -8,16 +8,21 @@ rospy.init_node("trajectory")
 
 joint_targets = rospy.Publisher('/joint_targets', JointState, queue_size=1)
 
-axis0 = [0,-math.pi/2]
-axis1 = [0,0]
-axis2 = [0,0.5]
+axis0 = [0,-math.pi/2,-math.pi/2,-math.pi/2]
+axis1 = [0,0,-math.pi/2,-math.pi/2]
+axis2 = [0,0,0,-math.pi/2]
 
-time = 5
+time = [5,10,15,0]
 sampleTime = 0.01
-samples = time/sampleTime
-axis0_increment = (axis0[1]-axis0[0])/samples
-axis1_increment = (axis1[1]-axis1[0])/samples
-axis2_increment = (axis2[1]-axis2[0])/samples
+axis0_increment = []
+axis1_increment = []
+axis2_increment = []
+
+for i in range(len(time)-1):
+    samples = (time[i+1]-time[i])/sampleTime
+    axis0_increment.append((axis0[i+1]-axis0[i])/samples)
+    axis1_increment.append((axis1[i+1]-axis1[i])/samples)
+    axis2_increment.append((axis2[i+1]-axis2[i])/samples)
 
 startTime = rospy.Time.now()
 
@@ -28,10 +33,14 @@ msg.velocity = [0,0,0]
 msg.effort = [0,0,0]
 
 rate = rospy.Rate(1/sampleTime)
-
-while not rospy.is_shutdown() and (rospy.Time.now()-startTime).to_sec()<time:
-    joint_targets.publish(msg)
-    rate.sleep()
-    msg.position[0] = msg.position[0] + axis0_increment
-    msg.position[1] = msg.position[1] + axis1_increment
-    msg.position[2] = msg.position[2] + axis2_increment
+checkpoint = 0
+while not rospy.is_shutdown() and checkpoint<len(time)-1:
+    if (rospy.Time.now()-startTime).to_sec()<time[checkpoint]:
+        joint_targets.publish(msg)
+        rate.sleep()
+        msg.position[0] = msg.position[0] + axis0_increment[checkpoint]
+        msg.position[1] = msg.position[1] + axis1_increment[checkpoint]
+        msg.position[2] = msg.position[2] + axis2_increment[checkpoint]
+    else:
+        checkpoint = checkpoint + 1
+rospy.loginfo("trajectory done")
