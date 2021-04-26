b973f2139a75bc6e5cdf63df7f18027fafc5bdb8
MoreBrain <nielsschlegel@web.de>
2021-04-26 15:31:39 +0200

    simplified code, changed lr string argument into boolean

--- scripts/hand_test.py
+++ scripts/hand_test.py
@@ -22,7 +22,7 @@ def open_hand():
     hand_msg.setpoint = [0]*len(hand_msg.global_id)
     motorcmd_pub.publish(hand_msg)
 
-def move_fingers(lr="right", targets = [0,0,0,0]):
+def move_fingers(right_hand = True, targets = [0,0,0,0]):
     """
     :param target: array of finger target values between 0 (open hand) and 800 (closed hand)
     lr: "left" or "right"
@@ -30,53 +30,52 @@ def move_fingers(lr="right", targets = [0,0,0,0]):
     right_ids = [42, 43, 44, 45]
     """
 
-    if lr == "right":
+    if right_hand:
         hand_msg.global_id = [42, 42, 44, 45]
-        hand_msg.setpoint = [i*800 for i in targets] # multiply all entries with 800 # 0 -> open hand; 800 -> closed hand
-        motorcmd_pub.publish(hand_msg)
-        rospy.sleep(0.07)
 
-    elif lr == "left":
+    else:
         hand_msg.global_id = [38, 39, 40, 41]
-        hand_msg.setpoint = [i*800 for i in targets]
-        motorcmd_pub.publish(hand_msg)
-        rospy.sleep(0.07)
 
-def move_fingers_simultaneous(lr = "right"):
+    hand_msg.setpoint = [i*800 for i in targets]
+    motorcmd_pub.publish(hand_msg)
+    rospy.sleep(0.07)
+
+def move_fingers_simultaneous(right_hand = True):
     # hand finger moving test all fingers move the same
     ranges = [0, 1]
     samples = 101
 
     for i in range(2):
         if not i%2:
-            print(lr, "close")
-            target_value = np.linspace(ranges[0], ranges[1], samples) # start with 0 (open) -> 1 (close)
+            print(right_hand, "close")
+            target_value = np.linspace(ranges[0], ranges[1], samples)  # start with 0 (open) -> 1 (close)
             for pos in target_value:
-                targets = [pos] * len(hand_msg.global_id) # put the current pos value in a 4-element list
-                move_fingers(lr, targets)
+                targets = [pos] * len(hand_msg.global_id)  # put the current pos value in a 4-element list
+                move_fingers(right_hand, targets)
         else:
-            print(lr, "open")
-            target_value = np.linspace(ranges[1], ranges[0], samples) # start with 1 -> 0
+            print(right_hand, "open")
+            target_value = np.linspace(ranges[1], ranges[0], samples)  # start with 1 -> 0
             for pos in target_value:
                 targets = [pos] * len(hand_msg.global_id)
-                move_fingers(lr, targets)
+                move_fingers(right_hand, targets)
 
-def move_pointer_finger(lr = "right"):
+def move_pointer_finger(right_hand = True):
     ranges = [0, 1]
     samples = 101
 
     for i in range(2):
         if not i%2:
-            print(lr, "close")
+            print(right_hand, "close")
             target_value = np.linspace(ranges[0], ranges[1], samples) # start with 0 (open) -> 1 (close)
             for pos in target_value:
-                targets = [0, pos, 0 , 0] # put the current pos value in a 4-element list
-                move_fingers(lr, targets)
+                targets = [0, pos, 0, 0] # put the current pos value in a 4-element list TODO: what is the pointer finger
+                move_fingers(right_hand, targets)
         else:
-            print(lr, "open")
+            print(right_hand, "open")
             target_value = np.linspace(ranges[1], ranges[0], samples) # start with 1 -> 0
             for pos in target_value:
                 targets = [0, pos, 0 , 0]
-                move_fingers(lr, targets)
+                move_fingers(right_hand, targets)
 
-move_fingers_simultaneous(lr = "right")
+move_pointer_finger(right_hand = True)
+#move_fingers_simultaneous(right_hand = True)
