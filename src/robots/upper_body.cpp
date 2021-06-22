77626f839e55e8fca7584a0307c19b8cf789e5f0
missxa <aliona.kharchenko@gmail.com>
2021-06-22 15:40:25 +0200

    removed de-init for wrists if a motor is down

--- src/robots/upper_body.cpp
+++ src/robots/upper_body.cpp
@@ -300,11 +300,13 @@ public:
                     //            communication_established[id] = true;
                 }
                 else {
-                    //            communication_established[id] = false;
-                    ROS_WARN_THROTTLE(10,"Did not receive motor status for motor with id: %d. %s Body part is disabled.", (id, body_part));
+                    if (body_part != "wrist_left" && body_part != "wrist_right")
+                    {
+                        //            communication_established[id] = false;
+                        ROS_WARN_THROTTLE(10,"Did not receive motor status for motor with id: %d. %s Body part is disabled.", (id, body_part));
 
-                    // TODO fix triceps
-                    //if (id != 18 && body_part != "shoulder_right") {
+                        // TODO fix triceps
+                        //if (id != 18 && body_part != "shoulder_right") {
                         if(init_called[body_part]) {
                             init_called[body_part] = false;
                             nh->setParam("initialized", init_called);
@@ -334,7 +336,9 @@ public:
                             }
 
                         }
-                            
+                    }
+
+
                     //}
 
                 }
